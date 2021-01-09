2143a377e3f9396f132a38c72d9535a37451fb0d
NevoStrauss <nevostrauss@gmail.com>
2021-01-09 11:44:37 +0200

    first2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sqlite3
 
+def connect():
+    return sqlite3.connect("database.db")
+
+def create_table(connection):
+    with connection:
+        connection.execute(
+            "CREATE TABLE vaccines(id INTEGER PRIMARY KEY, date DATE NOT NULL, supplier INTEGER, quantity INTEGER NOT NULL);"
+        )
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    connect()
