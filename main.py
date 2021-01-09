f2290b145f04183c068270fe52250ff0c698579c
NevoStrauss <nevostrauss@gmail.com>
2021-01-09 10:28:26 +0200

    first1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sqlite3
 
 def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
